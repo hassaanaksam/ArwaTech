92f58c4ebf496439f97f7b8f2b0aadcfe9f7e4f5
hassaanaksam <125266985+hassaanaksam@users.noreply.github.com>
2023-12-20 18:32:02 +0500

    Hero Api
    Hero Api Added

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const router = require("./src/router/employee-controller");
 const router2 = require("./src/router/field-controller")
+const routerHero = require("./src/router/hero-controller")
 const cors = require("cors");
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(express.json());
 app.use(cors());
 app.use("/employee", router);
 app.use("/hiretalent", router2)
+app.use("/hero", routerHero)
 
 mongoose.connect('mongodb+srv://hassaantahir:rock1122@mongocluster.j8ojcpq.mongodb.net/?retryWrites=true&w=majority').then(() => {
     console.log("Database connected..")
--- server/src/router/hero-controller.js
+++ server/src/router/hero-controller.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const multer = require("multer");
 const path = require("path");
-const Employee = require("../model/employee.js");
+const Hero = require("../model/hero.js");
 
-const router = express.Router();
+const routerHero = express.Router();
 
 // Set up Multer
 const storage = multer.diskStorage({
@@ -18,35 +18,34 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 // Serve static files
-router.use(express.static('uploads'));
+routerHero.use(express.static('uploads'));
 
-router.post("/employee", upload.single('image'), async (req, res) => {
+routerHero.post("/hero", upload.single('image'), async (req, res) => {
   try {
-    const employeesData = req.body;
+    const herosData = req.body;
     // const image = req.file.filename;
 
-    // const employee = new Employee({ ...employeesData, image });
-    const employee = new Employee(employeesData);
+    // const hero = new Hero({ ...herosData, image });
+    const hero = new Hero(herosData);
+    const heroData = await hero.save();
 
-    const employeeData = await employee.save();
-
-    res.status(200).send({ message: "Employee successfully saved", data: employeeData });
+    res.status(200).send({ message: "Hero successfully saved", data: heroData });
   } catch (error) {
     res.status(400).send(error);
   }
 });
 
-router.get('/employee', async (req, res) => {
+routerHero.get('/hero', async (req, res) => {
   try {
-    const employees = await Employee.find();
+    const heros = await Hero.find();
 
     res.json({
       status: true,
-      employees: employees
+      heros: heros
     });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 });
 
-module.exports = router;
+module.exports = routerHero;
